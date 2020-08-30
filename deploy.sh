5e099eee4a8be8931c4f4595d298e48b78dc3375
DrPsychick <drpsychick@drsick.de>
2020-08-30 04:05:46 +0200

    fix: extract paths (#7)
    generic tags

--- deploy.sh
+++ deploy.sh
@@ -23,15 +23,10 @@ if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
       -t $IMAGE:latest .
     docker push $IMAGE:latest
   else
-    # build and load it into local docker repository, so we can launch it and determine version
+    # build and push with correct version tag
+    echo "build and push docker image(s) for version $IMAGE:$UBUNTU_VERSION"
     docker build --build-arg UBUNTU_VERSION=$UBUNTU_VERSION \
-      -t $IMAGE .
-    export VERSION=$(eval $(docker run --rm --entrypoint grep $IMAGE ^version= linuxgsm.sh); echo $version)
-
-    # build again and push with correct version tag
-    echo "build and push docker image(s) for version $IMAGE:$VERSION-$UBUNTU_VERSION"
-    docker build --build-arg UBUNTU_VERSION=$UBUNTU_VERSION \
-      -t $IMAGE:$VERSION-$UBUNTU_VERSION .
-    docker push $IMAGE:$VERSION-$UBUNTU_VERSION
+      -t $IMAGE:$UBUNTU_VERSION .
+    docker push $IMAGE:$UBUNTU_VERSION
   fi
 fi
