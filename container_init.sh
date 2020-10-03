3eee66711a5e5c54ae38f64f9fecfa699d0858fc
DrPsychick <drpsychick@drsick.de>
2020-10-03 16:39:45 +0200

    refactor _saved directory (make _config obsolete)

--- container_init.sh
+++ container_init.sh
@@ -3,7 +3,9 @@
 # AFTER first install, BEFORE first run
 if [ ! -r ./serverfiles/ShooterGame/Saved ]; then
   mkdir -p ./serverfiles/ShooterGame/Saved \
-    && ln -s ../../../serverfiles_saved ./serverfiles/ShooterGame/Saved/SavedArks \
-    && ln -s ../../../serverfiles_config ./serverfiles/ShooterGame/Saved/Config \
+    && ln -s ../../../serverfiles_saved/SavedArks ./serverfiles/ShooterGame/Saved/SavedArks \
+    && ln -s ../../../serverfiles_saved/Config ./serverfiles/ShooterGame/Saved/Config \
+    && ln -s ../../../serverfiles_saved/Logs ./serverfiles/ShooterGame/Saved/Logs \
+    && ln -s ../../../serverfiles_saved/SaveGames ./serverfiles/ShooterGame/Saved/SaveGames \
     && ln -s ../../../serverfiles_clusters ./serverfiles/ShooterGame/Saved/clusters
 fi
