928590d95e9be79e4175d7b903611244620e72a5
DrPsychick <drpsychick@drsick.de>
2021-01-13 00:41:14 +0100

    LGSM creates Saved upon first install - replace it with symlinks

--- container_init.sh
+++ container_init.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # AFTER first install, BEFORE first run
-if [ ! -r ./serverfiles/ShooterGame/Saved ]; then
-  mkdir -p ./serverfiles/ShooterGame/Saved \
-    && ln -s ../../../serverfiles_saved/SavedArks ./serverfiles/ShooterGame/Saved/SavedArks \
-    && ln -s ../../../serverfiles_saved/Config ./serverfiles/ShooterGame/Saved/Config \
-    && ln -s ../../../serverfiles_saved/Logs ./serverfiles/ShooterGame/Saved/Logs \
-    && ln -s ../../../serverfiles_saved/SaveGames ./serverfiles/ShooterGame/Saved/SaveGames \
-    && ln -s ../../../serverfiles_clusters ./serverfiles/ShooterGame/Saved/clusters
+if [ ! -r ./serverfiles/ShooterGame/Saved/SavedArks -a ! -r ./serverfiles/ShooterGame/Saved/clusters ]; then
+  # remove directories created by lgsm
+  rm -rf ./serverfiles/ShooterGame/Saved
+  mkdir -p ./serverfiles/ShooterGame/Saved
+  ln -s ../../../serverfiles_saved/SavedArks ./serverfiles/ShooterGame/Saved/SavedArks
+  ln -s ../../../serverfiles_saved/Config ./serverfiles/ShooterGame/Saved/Config
+  ln -s ../../../serverfiles_saved/Logs ./serverfiles/ShooterGame/Saved/Logs
+  ln -s ../../../serverfiles_saved/SaveGames ./serverfiles/ShooterGame/Saved/SaveGames
+  ln -s ../../../serverfiles_clusters ./serverfiles/ShooterGame/Saved/clusters
 fi
